6529cffd78a467887f7c6905704682a35348658b
Onur Gunduz <onurboragunduz@gmail.com>
2021-04-25 00:39:50 -0400

    dropping markers

--- SkySecure/app/src/main/java/com/qureshizaeem/skysecure/MapActivity.java
+++ SkySecure/app/src/main/java/com/qureshizaeem/skysecure/MapActivity.java
@@ -7,21 +7,35 @@ import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.material.shape.MarkerEdgeTreatment;
 import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+//import com.mapbox.services.android.telemetry.location.LocationEngine;
+//import com.mapbox.services.android.telemetry.location.LocationEngineListener;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
+import java.util.List;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, /*LocationEngineListener,*/ PermissionsListener {
     private MapView mapView;
-    private MapboxMap map;
-    private PermissionsManager permissionsManager;
-    private LocationEngine locationEngine;
-    private LocationLayerPlugin locationLayerPlugin;
-    private Location originLocation;
+//    private PermissionsManager permissionsManager;
+//    private LocationEngine locationEngine;
+//    private LocationLayerPlugin locationLayerPlugin;
+//    private Location originLocation;
+
+    private Marker whiteHouseMarker;
+    private Point whiteHousePosition;
 
     public MapActivity() {
     }
@@ -46,7 +60,14 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
                         // Map is set up and the style has loaded. Now you can add data or make other map adjustments
 
+                        LatLng whiteHouseLatLng = new LatLng(38.8976763, -77.0365298);
 
+                        /*whiteHouseMarker = */mapboxMap.addMarker(new MarkerOptions().position(whiteHouseLatLng));
+
+                        //whiteHousePosition = Point.fromLngLat(whiteHouseLatLng.getLongitude(), whiteHouseLatLng.getLatitude());
+
+                        LatLng fbi = new LatLng(38.8952135, -77.0249586);
+                        mapboxMap.addMarker(new MarkerOptions().position(fbi));
                     }
                 });
 
@@ -93,5 +114,20 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+
     }
 }
